2720ddfa7a7944665977aa781d79ea0a9a499fee
GaodaGG <zil0718@outlook.com>
2024-10-17 21:43:05 +0800

    将数据与定位的初始化转交给Application

--- app/src/main/java/com/gg/busStation/MainApplication.java
+++ app/src/main/java/com/gg/busStation/MainApplication.java
@@ -2,6 +2,8 @@ package com.gg.busStation;
 
 import android.app.Application;
 
+import com.gg.busStation.function.DataBaseManager;
+import com.gg.busStation.function.location.LocationHelper;
 import com.google.android.material.color.DynamicColors;
 
 public class MainApplication extends Application {
@@ -11,5 +13,12 @@ public class MainApplication extends Application {
 
         //动态颜色
         DynamicColors.applyToActivitiesIfAvailable(this);
+
+        initData();
+    }
+
+    private void initData() {
+        DataBaseManager.initDB(this);
+        LocationHelper.init(this);
     }
 }
